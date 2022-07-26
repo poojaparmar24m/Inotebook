567877578c2351fa12e1652a455899cd694a9770
poojaparmar24m <pooja1parmar11@gmail.com>
2022-07-26 23:33:13 +0530

    updated api added

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -47,4 +47,37 @@ router.post(
   }
 );
 
+// Route 3: update an existing notes using :PUT "/api/notes//updateNote/:id".
+router.put("/updateNote/:id", fetchUser, async (req, res) => {
+  const { title, description, tag } = req.body;
+  // create a newNote object
+  const newNote = {};
+  if (title) {
+    newNote.title = title;
+  }
+  if (description) {
+    newNote.description = description;
+  }
+  if (tag) {
+    newNote.tag = tag;
+  }
+
+  // Find the note to be updated and update it
+  let note = await Note.findById(req.params.id);
+  if (!note) {
+    res.status(404).send("Not found");
+  }
+
+  if (note.user.toString() !== req.user.id) {
+    res.status(401).send("Not allowed");
+  }
+
+  note = await Note.findByIdAndUpdate(
+    req.params.id,
+    { $set: newNote },
+    { new: true }
+  );
+  res.json({ note });
+});
+
 module.exports = router;
